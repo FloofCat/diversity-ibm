8604122514343350a882faa11b7409721d2e5820
Floof <98069058+FloofCat@users.noreply.github.com>
2025-04-03 22:08:53 +0530

    :bug:

--- train/entropy.py
+++ train/entropy.py
@@ -12,7 +12,7 @@ class Entropy:
         tokens = self.tokenizer.encode(text, return_tensors="pt", truncation=True, max_length=1024)
         with torch.no_grad():
             outputs = self.model(tokens, labels=tokens)
-        log_probs = torch.log_softmax(outputs.logits, dim=-1).cuda()
+        log_probs = torch.log_softmax(outputs.logits.float(), dim=-1).cuda()
         token_log_probs = [log_probs[0, i, token].item() for i, token in enumerate(tokens[0][1:])]
             
         probs = [torch.exp(torch.tensor(log_prob)) for log_prob in token_log_probs]
