f8c0c322e8b3be3ffbfed8fdd8e6b63dd4256746
FloofCat <bitc3t@gmail.com>
2025-04-09 01:46:15 +0530

    Update rank.py

--- baseline_models/rank.py
+++ baseline_models/rank.py
@@ -18,5 +18,6 @@ class Rank:
         
         sorted_probs, indices = torch.sort(log_probs, descending=True)
         ranks = [(indices[0, i] == tokens[0][i + 1]).nonzero(as_tuple=True)[0].item() + 1 for i in range(len(tokens[0]) - 1)]
-        
+        if len(ranks) == 0:
+            return 0
         return sum(ranks) / len(ranks)
