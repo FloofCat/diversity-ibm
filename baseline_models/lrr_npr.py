0f399894c19e4e04d8b02d668f90b6c9ccbc118a
FloofCat <bitc3t@gmail.com>
2025-04-09 10:43:51 +0530

    :bug:

--- baseline_models/lrr_npr.py
+++ baseline_models/lrr_npr.py
@@ -38,7 +38,7 @@ class DetectLLM:
     def compute_LRR(self, text):
         token_log_probs, ranks = self.compute_log_probs_ranks(text)
         log_ranks = [torch.log(torch.tensor(rank, dtype=torch.float32)).item() for rank in ranks]
-        if(len(log_ranks) == 0):
+        if(len(log_ranks) == 0) or sum(log_ranks) == 0:
             return 0
         LRR = -sum(token_log_probs) / sum(log_ranks)
         return LRR
