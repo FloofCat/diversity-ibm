448d4b1f91f41ffba29a038304ef71b17644ed33
FloofCat <bitc3t@gmail.com>
2025-04-10 11:50:10 +0530

    Create radar_detector.py

---
+++ radar_detector.py
@@ -0,0 +1,44 @@
+import torch
+import numpy as np
+import gc
+from tqdm import tqdm
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from baseline_models.radar import RADAR
+
+class RadarWorker:
+    def __init__(self, model_path):
+        # Initialize once per actor
+        self.tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=False, trust_remote_code=True)
+        self.model = AutoModelForSequenceClassification.from_pretrained(
+            model_path, device_map="cuda:0", torch_dtype=torch.float16, trust_remote_code=True
+        )
+        
+        self.radar = RADAR(self.model, self.tokenizer)
+        
+    def infer(self, text):
+        return {
+            "roberta": self.radar.detect_probability(text)
+        }
+    
+    def infer_multiple(self, texts):
+        results = [None] * len(texts)
+        for i, text in enumerate(tqdm(texts)):
+            r = None
+            try:
+                r = self.radar.detect_probability(text)
+            except:
+                r = 0
+                
+            results[i] = {
+                "radar": r
+            }
+            
+        return results
+    
+    def shutdown(self):
+        del self.tokenizer
+        del self.model
+        gc.collect()
+        torch.cuda.empty_cache()
+    
+    
