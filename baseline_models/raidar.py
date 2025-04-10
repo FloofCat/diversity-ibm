e46871735150622def58e0d4b481fecc0d8b68f5
FloofCat <bitc3t@gmail.com>
2025-04-10 20:03:22 +0530

    :bug:

--- baseline_models/raidar.py
+++ baseline_models/raidar.py
@@ -12,7 +12,7 @@ class RAIDAR:
         system_prompt = "Concise this for me and keep all the information:"
         prompt = f"{system_prompt}\n\n{input_text}"
         
-        input_ids = self.tokenizer.encode(prompt, return_tensors="pt", max_length=1024, truncation=True)
+        input_ids = self.tokenizer.encode(prompt, return_tensors="pt", max_length=1024, truncation=True).to(self.model.device)
         output_ids = self.model.generate(input_ids, max_length=1024, num_return_sequences=1, temperature=0.0)
         rewritten_text = self.tokenizer.decode(output_ids[0], skip_special_tokens=True).strip()
         
